f7f3c02a509b46e8bd49b367de0021a789b25db4
delwende <birbaeliane@yahoo.fr>
2017-02-10 23:58:25 +0800

    new 2/10/2017

--- vues_globales/erreur_deja_connecte.php
+++ vues_globales/erreur_deja_connecte.php
@@ -1,3 +1,14 @@
-<h2>Accès interdit !</h2>
 
-<p>Inutile d'accéder à cette page si vous êtes connecté.</p>
+  <div id="content">
+					<div class="container">
+						<div class="row margin-vert-30">
+							<!-- Register Box -->
+							<div class="col-sm-8 col-sm-offset-2">
+								<div class="signup-page">
+									<h2>Accès interdit !</h2>
+									<p>Inutile d'accéder à cette page si vous êtes connecté.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
